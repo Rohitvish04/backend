9004f545f9de0dec25ebc68d5fc61c9dac2c0f40
Rohit Vishwakarma <143874936+Rohitvish04@users.noreply.github.com>
2025-01-18 18:11:13 +0000

    basic

---
+++ basic/server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+const data =["james"];
+
+
+//middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    
+  res.send(`<body style="background: gray;
+        color:white;">
+            <h1>DATA: </h1>
+            <p>${JSON.stringify(data)}</p>
+            <a href="/dashboard">Dashboard</a>
+        </body>`);
+});
+
+app.get("/dashboard", (req, res) => {
+  res.status(200).send(
+    `<body>
+    <h1>Dashboard Page</h1>
+    <a href="/">home</a>
+        </body>`);
+});
+
+app.get("/api/data", (req, res) =>{
+    res.send(data)
+});
+
+//POST 
+app.post("/api/data", (req, res) => {
+  const newEntry = req.body;
+  console.log(newEntry)
+  data.push(newEntry.name)
+  res.sendStatus(201)
+});
+
+//DELETE
+app.delete("/api/data", (req, res) =>{
+    data.pop()
+    console.log(`We deleted the element off the end of the array`);
+    res.sendStatus(203)
+    
+})
+ 
+
+app.listen(PORT, () => {
+  console.log(`Server started at port: ${PORT}`);
+});
